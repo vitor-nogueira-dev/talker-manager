b16058d0b7b88913928aca53e2eaa417f4d69241
Vitor Nogueira <vslnogueira96@gmail.com>
2023-04-15 09:55:19 -0300

    refactor: refatorando ifs validateRatePatch

--- src/middlewares/validateRatePatch.js
+++ src/middlewares/validateRatePatch.js
@@ -1,16 +1,9 @@
-const files = require('../files');
-
 const validateRatePatch = async (req, res, next) => {
   const { rate } = req.body;
-  if (rate === 0) {
-    return res.status(400).json({
-      message: 'O campo "rate" deve ser um número inteiro entre 1 e 5',
-    });
-  }
   if (!rate) {
     return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   }
-  if (!Number.isInteger(rate) || rate < 1 || rate > 5) {
+  if (!Number.isInteger(+rate) || +rate < 1 || +rate > 5) {
     return res.status(400).json({
       message: 'O campo "rate" deve ser um número inteiro entre 1 e 5',
     });
