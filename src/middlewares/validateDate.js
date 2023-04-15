701230dab936808d0613731226cde586155eb325
Vitor Nogueira <vslnogueira96@gmail.com>
2023-04-15 09:53:03 -0300

    refactor: refatorando if validateDate

--- src/middlewares/validateDate.js
+++ src/middlewares/validateDate.js
@@ -1,14 +1,9 @@
-const files = require('../files');
-
 const validateDate = async (req, res, next) => {
   const { date } = req.query;
-  if (date) {
-    const watchedAtRegex = /^\d{2}\/\d{2}\/\d{4}$/;
-    if (!watchedAtRegex.test(date)) {
-      return res.status(400).json({
-        message: 'O parâmetro "date" deve ter o formato "dd/mm/aaaa"',
-      });
-    }
+  if (date && !/^\d{2}\/\d{2}\/\d{4}$/.test(date)) {
+    return res.status(400).json({
+      message: 'O parâmetro "date" deve ter o formato "dd/mm/aaaa"',
+    });
   }
   return next();
 };
