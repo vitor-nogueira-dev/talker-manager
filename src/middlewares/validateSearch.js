871c7279279f35730ff1da29b6eb56168e8dd0fb
Vitor Nogueira <vslnogueira96@gmail.com>
2023-04-15 12:01:07 -0300

    fix: alterando cond dos ifs validateSearch

--- src/middlewares/validateSearch.js
+++ src/middlewares/validateSearch.js
@@ -1,8 +1,6 @@
 const validateSearch = (req, res, next) => {
   const { q, rate, date } = req.query;
-  console.log(q, rate, date, '3');
-  if (q && rate && date === undefined) {
-    console.log('to aqui');
+  if (q === undefined && rate === undefined && date === undefined) {
     return res.status(200).json([]);
   }
   return next();
