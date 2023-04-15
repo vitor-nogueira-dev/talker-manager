014936b3329340635e03e591b6b3cc0cdcae02ca
Vitor Nogueira <vslnogueira96@gmail.com>
2023-04-15 09:51:27 -0300

    refactor: refatorando if searchRate

--- src/middlewares/searchRate.js
+++ src/middlewares/searchRate.js
@@ -1,13 +1,9 @@
-const files = require('../files');
-
 const validateRate = async (req, res, next) => {
   const { rate } = req.query;
-  if (rate) {
-    if (!Number.isInteger(+rate) || +rate < 1 || +rate > 5) {
-      return res.status(400).json({
-        message: 'O campo "rate" deve ser um número inteiro entre 1 e 5',
-      });
-    }
+  if (rate && (!Number.isInteger(+rate) || +rate < 1 || +rate > 5)) {
+    return res.status(400).json({
+      message: 'O campo "rate" deve ser um número inteiro entre 1 e 5',
+    });
   }
   return next();
 };
