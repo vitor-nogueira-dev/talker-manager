835c37fe0c73be8c67f58f10a64b305e4cd6076d
Vitor Nogueira <vslnogueira96@gmail.com>
2023-04-15 12:00:36 -0300

    fix: alterando cond do if para undefined

--- src/middlewares/validateRatePatch.js
+++ src/middlewares/validateRatePatch.js
@@ -1,6 +1,6 @@
 const validateRatePatch = async (req, res, next) => {
   const { rate } = req.body;
-  if (!rate) {
+  if (rate === undefined) {
     return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   }
   if (!Number.isInteger(+rate) || +rate < 1 || +rate > 5) {
