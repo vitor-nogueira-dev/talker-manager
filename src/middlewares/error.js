3e8ca67636967b8ae7bb535188029d57a067af0e
Vitor Nogueira <vslnogueira96@gmail.com>
2023-04-15 12:07:35 -0300

    refactor: refatorando funções, lint, arquivos

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,6 +1,5 @@
 const errorHandle = (err, _req, res, _next) => {
   const { statusCode, message } = err;
-  console.log(message, 'err')
   return res
     .status(statusCode || 500)
     .json({ message } || { message: 'Error captured in express' });
